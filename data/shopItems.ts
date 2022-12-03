904e27611fa3c773bb817fcfa59b832d577eca2e
DongHyun <hyde981114@gmail.com>
2022-12-04 01:13:49 +0900

    feat: shopItems 더미 데이터 추가

---
+++ data/shopItems.ts
@@ -0,0 +1,76 @@
+export interface ShopItemType {
+  id: string;
+  shop_img: string;
+  shop_name: string;
+  shop_operating_hours: string;
+  shop_tel: string;
+}
+
+const shopItems: ShopItemType[] = [
+  {
+    id: '63494f40b484e885502ea8f5',
+    shop_img:
+      'https://media.istockphoto.com/photos/happy-couple-with-their-puppies-buying-toys-and-dog-food-in-pet-shop-picture-id1370565928?k=20&m=1370565928&s=612x612&w=0&h=XM4ZH_YCpH_J48hbH5uNVBUEUXU7eSOik693wY4L6qw=',
+    shop_name: '뷰티 애견 살롱',
+    shop_operating_hours: '10:00 ~ 20:00',
+    shop_tel: '053-321-3214'
+  },
+  {
+    id: '634fb9d3509b3fc88d8bbea0',
+    shop_img: 'https://cdn.pixabay.com/photo/2016/11/18/19/13/buildings-1836478_1280.jpg',
+    shop_name: '쿨 애견 살롱',
+    shop_operating_hours: '08:00 ~ 19:00',
+    shop_tel: '053-000-1234'
+  },
+  {
+    id: '6363693b05ef54530669cf7d',
+    shop_img: 'https://cdn.pixabay.com/photo/2014/07/15/13/36/coffee-shop-393954_1280.jpg',
+    shop_name: '카와이 애견 살롱',
+    shop_operating_hours: '10:00 ~ 20:00',
+    shop_tel: '053-678-1234'
+  },
+  {
+    id: '63636b0c05ef54530669cf81',
+    shop_img: 'https://cdn.pixabay.com/photo/2017/03/22/17/37/barber-2165745_1280.jpg',
+    shop_name: '피카 살롱',
+    shop_operating_hours: '12:00 ~ 20:00',
+    shop_tel: '053-898-9090'
+  },
+  {
+    id: '63636bd905ef54530669cf85',
+    shop_img: 'https://cdn.pixabay.com/photo/2015/11/13/07/47/italy-1041660_1280.jpg',
+    shop_name: '씨엘 애견 살롱',
+    shop_operating_hours: '09:00 ~ 19:00',
+    shop_tel: '053-876-4321'
+  },
+  {
+    id: '63494f40b484e885502ea8t4',
+    shop_img: 'https://cdn.pixabay.com/photo/2016/11/19/15/40/clothes-1839935_1280.jpg',
+    shop_name: '말부 애견 살롱',
+    shop_operating_hours: '10:00 ~ 20:00',
+    shop_tel: '053-321-3214'
+  },
+  {
+    id: '63494f40b484e885502ea8q2',
+    shop_img: 'https://cdn.pixabay.com/photo/2022/03/27/10/18/market-7094635_1280.jpg',
+    shop_name: '로컬 애견 살롱',
+    shop_operating_hours: '10:00 ~ 20:00',
+    shop_tel: '053-321-3214'
+  },
+  {
+    id: '63494f40b484e885502ea8w1',
+    shop_img: 'https://cdn.pixabay.com/photo/2019/03/09/21/30/downtown-4045036_1280.jpg',
+    shop_name: '클로이 애견 살롱',
+    shop_operating_hours: '10:00 ~ 20:00',
+    shop_tel: '053-321-3214'
+  },
+  {
+    id: '63494f40b484e885502ea8c1',
+    shop_img: 'https://cdn.pixabay.com/photo/2020/04/11/14/55/shops-5030731_1280.jpg',
+    shop_name: '클로이 애견 살롱',
+    shop_operating_hours: '10:00 ~ 20:00',
+    shop_tel: '053-321-3214'
+  }
+];
+
+export default shopItems;
