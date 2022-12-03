cbfd8f9490a1e811e94ded30012dbb306bc0d872
DongHyun <hyde981114@gmail.com>
2022-12-04 02:29:00 +0900

    feat: Infinity Scroll 구현
    Intersection Observer API를 사용하여 Infinity Scroll 구현

--- components/UI/organisms/ShopItemListSection.tsx
+++ components/UI/organisms/ShopItemListSection.tsx
@@ -1,17 +1,26 @@
 import ShopItemBox from '../molecules/ShopItemBox';
 import ShopItemSkeletonBox from '../molecules/ShopItemSkeletonBox';
 
+import useInfinityScroll from '../../../hooks/useInfinityScroll';
+
 import type { ShopItemType } from '../../../data/shopItems';
 
 const ShopListSection = ({ shopItems }: { shopItems: ShopItemType[] }) => {
+  const { currentList, isFetchedDone, observerTargetEl } = useInfinityScroll({
+    initialList: shopItems,
+    fetchType: 'shopItems',
+    fetchUrl: 'http://localhost:3001/api/fetchShopItems',
+    fetchLength: 3
+  });
+
   return (
     <>
-      {shopItems.map((shopItem) => (
+      {currentList.map((shopItem) => (
         <li key={shopItem.id}>
           <ShopItemBox shopItem={shopItem} />
         </li>
       ))}
-      <ShopItemSkeletonBox />
+      {!isFetchedDone && <ShopItemSkeletonBox ref={observerTargetEl} />}
     </>
   );
 };
---
+++ hooks/useInfinityScroll.ts
@@ -0,0 +1,56 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+import type { ShopItemType } from '../data/shopItems';
+
+interface ShopItemsInfinityScroll {
+  initialList: ShopItemType[];
+  fetchType: 'shopItems';
+  fetchUrl: string;
+  fetchLength: number;
+}
+
+const useInfinityScroll = (infinityScrollConfig: ShopItemsInfinityScroll) => {
+  const { initialList, fetchType, fetchUrl, fetchLength } = infinityScrollConfig;
+
+  const [currentList, setCurrentList] = useState(initialList);
+  const [isFetchedDone, setIsFetchedDone] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    const res = await fetch(fetchUrl, {
+      method: 'POST',
+      body: JSON.stringify({ fetchStartIndex: currentList.length, fetchLength: fetchLength }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+
+    const { fetchedShopItems, fetchedDone } = await res.json();
+
+    setCurrentList((prevList) => [...prevList, ...fetchedShopItems]);
+    setIsFetchedDone(fetchedDone);
+  }, [currentList, fetchUrl, fetchLength]);
+
+  const onIntersect: IntersectionObserverCallback = useCallback(
+    async ([entry]) => {
+      if (entry.isIntersecting && !isFetchedDone) {
+        fetchData();
+      }
+    },
+    [isFetchedDone, fetchData]
+  );
+
+  const observerTargetEl = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    if (!observerTargetEl.current) return;
+
+    const observer = new IntersectionObserver(onIntersect, { threshold: 0.8 });
+    observer.observe(observerTargetEl.current);
+
+    return () => observer.disconnect();
+  }, [onIntersect]);
+
+  return { currentList, isFetchedDone, observerTargetEl };
+};
+
+export default useInfinityScroll;
