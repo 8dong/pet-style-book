8716e3ce733cfebac5e5c011231ffe6f54e95c7c
DongHyun <hyde981114@gmail.com>
2022-12-05 01:16:11 +0900

    feat: fetchShopItems & fetchStyleItems API 수정
    요청 객체의 몸체(req.body)에 fetchLength가 없는 경우(undefined)
    모든 아이템을 응답으로 전달

--- pages/api/fetchShopItems.ts
+++ pages/api/fetchShopItems.ts
@@ -6,6 +6,13 @@ const fetchShopItems: NextApiHandler = async (req, res) => {
   const fetchStartIndex = req.body.fetchStartIndex;
   let fetchEndIndex = fetchStartIndex + req.body.fetchLength;
 
+  if (req.body.fetchLength === undefined) {
+    const fetchedShopItems = shopItems.slice();
+
+    res.status(200).json({ fetchedShopItems, fetchedDone: true });
+    return;
+  }
+
   if (fetchEndIndex >= shopItems.length) {
     fetchEndIndex = shopItems.length;
     const fetchedShopItems = shopItems.slice(fetchStartIndex, fetchEndIndex);
--- pages/api/fetchStyleItems.ts
+++ pages/api/fetchStyleItems.ts
@@ -6,6 +6,13 @@ const fetchShopItems: NextApiHandler = async (req, res) => {
   const fetchStartIndex = req.body.fetchStartIndex;
   let fetchEndIndex = fetchStartIndex + req.body.fetchLength;
 
+  if (req.body.fetchLength === undefined) {
+    const fetchedStyleItems = styleItems.slice();
+
+    res.status(200).json({ fetchedStyleItems, fetchedDone: true });
+    return;
+  }
+
   if (fetchEndIndex >= styleItems.length) {
     fetchEndIndex = styleItems.length;
     const fetchedStyleItems = styleItems.slice(fetchStartIndex, fetchEndIndex);
